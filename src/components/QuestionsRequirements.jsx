8878c1d5ed29464516c073e7dd9350b3e22238d8
WonderSi <kirillkozlov00@gmail.com>
2024-04-22 11:55:10 +0700

    Creature QuestionsRequirements 1.0.0

--- src/components/QuestionsRequirements.jsx
+++ src/components/QuestionsRequirements.jsx
@@ -2,7 +2,27 @@ import styles from "../style"
 
 const QuestionsRequirements = () => (
   <section>
+    <div>
+      <div>
+        <h1 className={`${styles.heading1}`}>Условия и 
+часто задаваемые вопросы</h1>
+        <p>В случае любой нестандартной ситуации, включая ДТП, можете рассчитывать на нашу помощь. Мы своих в беде не бросаем!</p>
+      </div>
 
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
   </section>
 )
 
